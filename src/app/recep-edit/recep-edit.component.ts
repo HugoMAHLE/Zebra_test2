6370935d216e73cf643191b2513be33dc25aba4b
TellezJr24 <daniel.a.tellez@mahle.com>
2024-12-13 13:59:33 -0700

    The end?
    Finalizacion, en caso de no haber cambios, de Front End

---
+++ src/app/recep-edit/recep-edit.component.ts
@@ -0,0 +1,46 @@
+import { Component, signal, ChangeDetectionStrategy } from '@angular/core';
+import { FormControl, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { merge } from 'rxjs';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+@Component({
+  selector: 'app-recep-edit',
+  standalone: true,
+  imports: [ MatCardModule, MatButtonModule, MatFormFieldModule, FormsModule, ReactiveFormsModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: './recep-edit.component.html',
+  styleUrl: './recep-edit.component.css'
+})
+export class RecepEditComponent {
+
+  readonly email = new FormControl('', [Validators.required, Validators.email]);
+  
+  
+  errorMessage = signal('');
+
+  constructor() {
+    merge(this.email.statusChanges, this.email.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateErrorMessage());
+  }
+
+  updateErrorMessage() {
+    if (this.email.hasError('required')) {
+      this.errorMessage.set('You must enter a value');
+    } else if (this.email.hasError('email')) {
+      this.errorMessage.set('Not a valid email');
+    } else {
+      this.errorMessage.set('');
+    }
+  }
+
+
+  protected readonly value = signal('');
+
+  protected onInput(event: Event) {
+    this.value.set((event.target as HTMLInputElement).value);
+  }
+}
