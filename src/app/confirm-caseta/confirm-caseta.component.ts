e3c7550303d0d6103fe94460c19c2234a5ddd8dd
TellezJr24 <daniel.a.tellez@mahle.com>
2025-01-28 10:24:42 -0700

    Ultimos cambios
    Se hicieron los ultimos cambios de diseño en las paginas

--- src/app/confirm-caseta/confirm-caseta.component.ts
+++ src/app/confirm-caseta/confirm-caseta.component.ts
@@ -2,11 +2,13 @@ import { Component, inject } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import { Router } from 'express';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-confirm-caseta',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, MatIconModule, MatToolbarModule],
   templateUrl: './confirm-caseta.component.html',
   styleUrl: './confirm-caseta.component.css'
 })
@@ -19,5 +21,12 @@ export class ConfirmCasetaComponent {
 
   navEdit(){
     this.router.navigate(["/reception/edit-reception"])
+  }
+
+  logOff() {
+    console.log("sesion terminada");
+    localStorage.removeItem("angular18Local");
+    alert("Sesión cerrada");
+    this.router.navigate(['/login']);
   }
 }
--- src/app/print/print.component.ts
+++ src/app/print/print.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-print',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, MatIconModule, MatToolbarModule],
   templateUrl: './print.component.html',
   styleUrl: './print.component.css'
 })
 export class PrintComponent {
+  router = inject(Router);
 
+  logOff() {
+    console.log("sesion terminada");
+    localStorage.removeItem("angular18Local");
+    alert("Sesión cerrada");
+    this.router.navigate(['/login']);
+  }
+
+  navReception(){
+    this.router.navigate(["/reception"]);
+  }
 }
